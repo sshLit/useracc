a2c69736f517ae0aaa98c3e363b27dbd7e3346f6
sshLit <scrappy1337@icloud.com>
2025-02-26 14:19:55 +0300

    fet: создан стор, пернесена вся логика в стор. немного подкрутил правки. УБрал часть коментов (можно посмотреть в прошлом комите)

--- src/stores/accounts.ts
+++ src/stores/accounts.ts
@@ -1,1 +1,48 @@
-// Пока пусто, сделаем позже стор
+import { defineStore } from 'pinia';
+// Все равно без localstorage не обойтись, есть несколько решений, но они долгие + не нужные
+// 1. Можем хранить в памяти pinia, но если обновим страницу - данные удаляться
+// 2. Можем цепляться за сессию, но а зачем? если браузер закроем и откроем заново то данных не будет
+// 3. Можно через куки, но сложно + долго реализовывать (мало времени)
+// 4. Хранить на сервере данные.
+export const useAccountsStore = defineStore('accounts', {
+  state: () => ({
+    accounts: JSON.parse(localStorage.getItem('accounts') || '[]')
+  }),
+
+  actions: {
+    addAccount() {
+      this.accounts.push({ type: 'LDAP', login: '', password: null, label: '' });
+      this.saveToLocalStorage();
+    },
+
+    removeAccount(index: number) {
+      this.accounts.splice(index, 1);
+      this.saveToLocalStorage();
+    },
+
+    updateLabel(index: number, label: string) {
+      this.accounts[index].label = label
+        .split(';')
+        .map(l => l.trim())
+        .filter(l => l !== '');
+
+      this.saveToLocalStorage();
+    },
+
+    updateType() {
+      this.saveToLocalStorage();
+    },
+
+    validateAccount(index: number) {
+      const account = this.accounts[index];
+      account.touched = true;
+      account.validLogin = account.login.trim() !== '';
+      account.validPassword = account.type === 'Local' ? account.password.trim() !== '' : true;
+      this.saveToLocalStorage();
+    },
+
+    saveToLocalStorage() {
+      localStorage.setItem('accounts', JSON.stringify(this.accounts));
+    }
+  }
+});
--- src/views/AccountsViews.vue
+++ src/views/AccountsViews.vue
@@ -23,19 +23,19 @@
               <q-select
                 v-model="account.type"
                 :options="['LDAP', 'Local']"
-                @update:model-value="updateType(index)"
+                @update:model-value="updateType()"
                 dense
               />
             </div>
+            <!-- Аналогично, здесь мы так же используем блур и делаем валидации -->
             <div class="col field">
-              <!-- Аналогично, здесь мы так же используем блур и делаем валидации -->
               <q-input
                 v-model="account.login"
                 label="Логин"
                 maxlength="100"
                 @blur="validateAccount(index)"
                 :error="!account.validLogin && account.touched"
-                error-message="Поле обязательнео к вводу"
+                error-message="Поле обязательно к вводу"
                 dense
               />
             </div>
@@ -48,11 +48,11 @@
                 maxlength="100"
                 @blur="validateAccount(index)"
                 :error="!account.validPassword && account.touched"
-                error-message="Поле обязательнео к вводу"
+                error-message="Поле обязательно к вводу"
                 dense
               />
-            </div>
-            <q-btn flat round dense color="negative" icon="delete" class="delete-btn" @click="removeAccount(index)" />
+              </div>
+              <q-btn flat round dense color="negative" icon="delete" class="delete-btn" @click="removeAccount(index)" />
           </q-card-section>
         </q-card>
       </div>
@@ -64,100 +64,50 @@
 
 <script lang="ts">
 // И так обычно я это не делаю. Но в качестве тестового задания распишу, что за что отвечает и как работает
-import { ref, onMounted } from 'vue'; // Здесь подключаем реактивность и хук маунтед для загрузки данных
-import type { Account } from "src/types/types"; // Здесь уже работаем с нашим типом данных. В прошлом комите мы это описали
+import { computed, ref, watch } from 'vue'; // Здесь подключаем реактивность вотчер и компьютед свойствва. мантед больше не нужен
+import { useAccountsStore } from 'src/stores/accounts'; // Наш стор
+import type { Account } from 'src/types/types'; // Здесь уже работаем с нашим типом данных. В прошлом комите мы это описали
 
 export default {
   setup() {
-    const accounts = ref<Account[]>([]);
-    const labelInputs = ref<string[]>([]); // времянка
+    const store = useAccountsStore();  // Получаем данные из нашего стора
+    const accounts = computed(() => store.accounts); // А теперь их вставляем
+    // Наши метки, через ;
+    const labelInputs = ref<string[]>(accounts.value.map((acc: Account) =>
+      // eslint-disable-next-line @typescript-eslint/no-base-to-string
+      Array.isArray(acc.label) ? acc.label.join('; ') : acc.label ?? ''
+    ));
 
-    // Наш маунтед, получаем пока из локал стора, если успею переведу на pinia
-    // TODO: Pinia
-    onMounted(() => {
-      const storedAccounts = localStorage.getItem('accounts');
-      if (storedAccounts) {
-        accounts.value = JSON.parse(storedAccounts);
+    watch(accounts, (newAccounts: Account[]) => {
+      labelInputs.value = newAccounts.map(acc =>
+        // eslint-disable-next-line @typescript-eslint/no-base-to-string
+        Array.isArray(acc.label) ? acc.label.join('; ') : acc.label ?? ''
+      );
+    }, { deep: true });
 
-        // чекаем, является ли acc.label массивом, если нет, то преобразуем
-        labelInputs.value = accounts.value.map(acc =>
-          Array.isArray(acc.label)
-            ? acc.label.map(l => l.text).join('; ')
-            : acc.label ?? '' // если нет, то просто используем значение или пустую строку
-        );
-      }
-    });
 
+    // Сохраняем запись в стор
     const addAccount = () => {
-      accounts.value.push({
-        // Добавляем новый объект в массив accounts
-        label: [],
-        type: 'LDAP',
-        login: '',
-        password: null,
-        validLogin: false,
-        validPassword: false,
-        touched: false, // Правили запись или нет? Без него во всех строках упремся в валидацию
-      });
+      store.addAccount();
       labelInputs.value.push('');
     };
-
+    // делит запись из стора
     const removeAccount = (index: number) => {
-      // Удаляем элемент из массива accounts по индексу. Вообще за такое нужно бить по рукам разработчику
-      // т.к. если приложение разрастется, то в дальнейшем могут быть проблемы, при удалении из массива по индексу
-      accounts.value.splice(index, 1);
+      store.removeAccount(index);
       labelInputs.value.splice(index, 1);
-      saveAccounts(); // Сейв на всякий случай
+    };
+    // если чет поменяли то апдейтим
+    const updateType = () => {
+      store.updateType();
     };
 
-    // Пробрасываем пароль null если ldap пользователь
-    const updateType = (index: number) => {
-      if (!accounts.value[index]) {
-        return;
-      }
-
-      if (accounts.value[index].type === 'LDAP') {
-        accounts.value[index].password = null;
-      }
-
-      validateAccount(index);
-      saveAccounts();
-    };
-
+    // проверяем валидацию
     const validateAccount = (index: number) => {
-      // Функция для валидации аккаунта
-      const account = accounts.value[index];
-
-      if (!account) {
-        return; // защищаем  от undf..
-      }
-
-      account.touched = true;
-      account.validLogin = !!account.login;
-      account.validPassword = account.type === 'LDAP' || !!account.password;
-
-      saveAccounts();
+      store.validateAccount(index);
     };
-
-    // Блур для наших меток, после ввода преобразуем их в массив разделенный по ;
+    // Наш блур
     const handleLabelBlur = (index: number) => {
-      const rawLabel = labelInputs.value[index] ? labelInputs.value[index].trim() : '';
-      const labelsArray = rawLabel.length > 0 ? rawLabel.split(';').map(label => label.trim()) : [];
-
-      if (accounts.value[index]) {
-        accounts.value[index].label = labelsArray
-          .filter(label => label.length > 0)
-          .map(label => ({ text: label }));
-        accounts.value[index].touched = true;
-      }
-
-      saveAccounts();
-    };
-
-    const saveAccounts = () => {
-      localStorage.setItem('accounts', JSON.stringify(accounts.value));
-      console.log(localStorage.getItem('accounts'));
-      // Сохраняем аккаунты в localStorage
+      store.updateLabel(index, labelInputs.value[index] ?? '');
     };
 
     return {
@@ -205,8 +155,8 @@ export default {
   font-size: 18px;
   font-weight: 400;
 }
+
 .q-select {
   margin-bottom: 20px;
 }
 </style>
-
